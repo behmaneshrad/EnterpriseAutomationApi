98642c419d2a11ebb890f2fcec38de4f2116e215
Erisa Ehsani <ehsanierisa9@gmail.com>
2025-07-22 00:54:22 +0330

    Add BaseEntity with common entity properties

---
+++ EnterpriseAutomation.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace EnterpriseAutomation.Domain.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }         
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;  
+        public DateTime? UpdatedAt { get; set; }                    
+
+        public bool IsDeleted { get; set; } = false;             
+    }
+}
