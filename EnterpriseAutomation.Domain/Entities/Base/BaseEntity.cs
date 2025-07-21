5f415cb79eabd11a0474c2ad3fa474fd3120c757
Hossein Molaei <152036186+hosseinmolaeibackend@users.noreply.github.com>
2025-07-22 00:57:01 +0330

    Update BaseEntity.cs

--- EnterpriseAutomation.Domain/Entities/Base/BaseEntity.cs
+++ EnterpriseAutomation.Domain/Entities/Base/BaseEntity.cs
@@ -1,8 +1,7 @@
 ﻿namespace EnterpriseAutomation.Domain.Entities.Base
 {
     public abstract class BaseEntity
-    {
-        public int Id { get; set; }         
+    {   
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;  
         public DateTime? UpdatedAt { get; set; }                    
