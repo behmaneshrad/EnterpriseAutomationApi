fe6816c6018dcc92642f15494d41e4d0b9ac3a76
coretech-AI <somayehshamaeizadeh1360@gmail.com>
2025-07-22 01:14:37 +0330

    Refactor: Inherit Request from Base class

--- EnterpriseAutomation.Domain/Entities/Request.cs
+++ EnterpriseAutomation.Domain/Entities/Request.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EnterpriseAutomation.Domain.Entities.Base;
 namespace EnterpriseAutomation.Domain.Entities
 {
-    public class Request
+    public class Request:Base
     {
         public string Id { get; set; } = default!;
         public string Title { get; set; } = default!;
         public string Description { get; set; } = default!;
         public string CreatedByUserId { get; set; } = default!;
-        public DateTime CreatedAt { get; set; } = default!;
+        //public DateTime CreatedAt { get; set; } = default!;
         public string CurrentStatus { get; set; } = default!;
         public string CurrentStep { get; set; } = default!;
         public string WorkflowId { get; set; } = default!;
