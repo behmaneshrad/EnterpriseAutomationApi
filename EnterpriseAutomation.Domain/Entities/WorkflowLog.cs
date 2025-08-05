ff310dbe6a3778e95cd1a0fef10e46eea2eb1398
Iman Khalilorahmani <imankhtech@gmail.com>
2025-08-05 10:31:34 +0330

    workflowlog and actiontype added to domain

---
+++ EnterpriseAutomation.Domain/Entities/WorkflowLog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using EnterpriseAutomation.Domain.Entities.Base;
+using EnterpriseAutomation.Domain.Enums;
+
+namespace EnterpriseAutomation.Domain.Entities
+{
+    [Display(Name = "لاگ گردش کار")]
+    public class WorkflowLog : BaseEntity
+    {
+        [Key]
+        [Display(Name = "آی دی لاگ")]
+        public int Id { get; set; }
+
+
+        [Display(Name = "آی دی گردش کار")]
+        public required int WorkflowId { get; set; }
+
+        [Display(Name = "آی دی مرحله گردش کار")]
+        public required int StepId { get; set; }
+
+        [Display(Name = "آی دی درخواست دهنده")]
+        public required int UserId { get; set; }
+
+        [Display(Name = "نام درخواست دهنده")]
+        public required string UserName { get; set; }
+
+        [Display(Name = "نوع عملیات")]
+        public ActionType ActionType { get; set; }
+
+        [Display(Name = "توضیحات")]
+        public string ? Description { get; set; }
+
+        [Display(Name = "شماره درخواست")]
+        public required int RequestId { get; set; }
+
+        [Display(Name = "وضعیت سابق")]
+        public string? PreviousState { get; set; }
+
+        [Display(Name = "وضعیت جدید")]
+        public string? NewState { get; set; }
+        
+    }
+}
---
+++ EnterpriseAutomation.Domain/Enums/ActionType.cs
@@ -0,0 +1,16 @@
+﻿namespace EnterpriseAutomation.Domain.Enums
+{
+    public enum ActionType
+    {
+        Approve = 1,
+        Reject = 2,
+        Skip = 3,
+        Assign = 4,
+        Start = 5,
+        Complete = 6,
+        Cancel = 7,
+        Pause = 8,
+        Resume = 9,
+        Update = 10
+    }
+}
