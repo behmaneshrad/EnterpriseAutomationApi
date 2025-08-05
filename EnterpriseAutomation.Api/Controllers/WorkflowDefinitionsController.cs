29ab16689e2bf78ee041c1250cb5952366dcc39d
HosseinMolaei <hossein.molaei.backend@gmail.com>
2025-08-05 16:05:49 +0330

    Add service for workflow add controller for work flow

---
+++ EnterpriseAutomation.Api/Controllers/WorkflowDefinitionsController.cs
@@ -0,0 +1,20 @@
+﻿using EnterpriseAutomation.Api.Controllers.BaseController;
+using EnterpriseAutomation.Application.IRepository;
+using EnterpriseAutomation.Application.WorkflowDefinitions.Interfaces;
+using EnterpriseAutomation.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseAutomation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkflowDefinitionsController : BaseController<WorkflowDefinition>
+    {
+        private readonly IWorkflowDefinitionsService _definitionsService;
+        public WorkflowDefinitionsController(IRepository<WorkflowDefinition> repository, IWorkflowDefinitionsService definitionsService) : base(repository)
+        {
+            _definitionsService = definitionsService;
+        }
+    }
+}
---
+++ EnterpriseAutomation.Application/WorkflowDefinitions/Interfaces/IWorkflowDefinitionsService.cs
@@ -0,0 +1,20 @@
+﻿using EnterpriseAutomation.Application.WorkflowDefinitions.Models;
+using EnterpriseAutomation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseAutomation.Application.WorkflowDefinitions.Interfaces
+{
+    public interface IWorkflowDefinitionsService
+    {
+        public Task<IEnumerable<WorkflowDefinitionDto>> GetAllWorkflowDefinitionsAsync();
+
+        public Task<WorkflowDetailDto> GetWorkFlowById(int id);
+
+        public Task AddWorkflowDefinition(WorkflowDefinitionDto wfDto);
+        public Task DeleteWorkflowDefinition(int id);
+    }
+}
