87a39d4a37a06ffada6af7fe98629985a7bff028
Erisa Ehsani <ehsanierisa9@gmail.com>
2025-08-18 00:49:37 +0330

    untracked files on main: 949a1c8 #231 : Handle Authorize with keycloak

---
+++ EnterpriseAutomation.Application/Requests/Models/ApproveRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EnterpriseAutomation.Application.Requests.Models
+{
+    public class ApproveRequestDto
+    {
+        /// true -> تایید & false -> رد
+        public bool IsApproved { get; set; }
+
+        /// توضیحات/کامنت کاربر تأیید کننده
+        public string? Comment { get; set; }
+    }
+}
