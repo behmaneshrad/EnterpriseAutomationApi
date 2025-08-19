8fdc5f18f3ea26e0f58f1f9bdd9ad4e4fba56e87
Erisa Ehsani <ehsanierisa9@gmail.com>
2025-08-19 15:34:41 +0330

    Restore files mistakenly deleted by previous commit

---
+++ EnterpriseAutomation.Domain/Entities/Permission.cs
@@ -0,0 +1,27 @@
+﻿using EnterpriseAutomation.Domain.Entities.Base;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace EnterpriseAutomation.Domain.Entities;
+
+[Display(Name = "دسترسی ها")]
+public class Permission : BaseEntity
+{
+    [Key]
+    [Display(Name = "آیدی")]
+    public int PermissionId { get; set; }
+
+    [Display(Name = "اسم api")]
+    public string Key { get; set; } = default!;
+
+    [Display(Name = "اسم اکشن")]
+    public string? Name { get; set; }
+
+    [Display(Name = "توضیحات")]
+    public string? Description { get; set; }
+
+    [Display(Name = "فعال بودن")]
+    public bool IsActive { get; set; } = true;
+
+    public ICollection<RolePermissions> Roles { get; set; } = [];
+}
