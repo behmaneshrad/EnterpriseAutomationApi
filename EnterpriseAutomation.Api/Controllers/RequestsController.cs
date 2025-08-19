8f195bbd01fa5ca88bcda28bd243abd333204bb5
Erisa Ehsani <ehsanierisa9@gmail.com>
2025-08-19 15:12:31 +0330

    Restore from dropped stash c0ad5e6 (excluding Migrations/Snapshot)

--- EnterpriseAutomation.Api/Controllers/RequestsController.cs
+++ EnterpriseAutomation.Api/Controllers/RequestsController.cs
@@ -31,7 +31,6 @@ namespace EnterpriseAutomation.API.Controllers
         //[Authorize]
         //[RequiresPermission("requests", "create")]
         [HttpPost("create")] // حتماً متد HTTP مشخص باشد
-        [Authorize("HasAccess")]
         public async Task<IActionResult> CreateRequest([FromBody] CreateRequestDto dto)
         {
             if (!ModelState.IsValid)
