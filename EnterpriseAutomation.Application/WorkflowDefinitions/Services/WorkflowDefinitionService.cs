1e7089185e7fe64de1d7f8b94d5111b35580174c
HosseinMolaei <hossein.molaei.backend@gmail.com>
2025-09-06 12:16:45 +0330

    #1404-06-15: Deleted logger folder

--- EnterpriseAutomation.Application/WorkflowDefinitions/Services/WorkflowDefinitionService.cs
+++ EnterpriseAutomation.Application/WorkflowDefinitions/Services/WorkflowDefinitionService.cs
@@ -9,11 +9,7 @@ using EnterpriseAutomation.Infrastructure.Utilities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace EnterpriseAutomation.Application.WorkflowDefinitions.Services
 {
