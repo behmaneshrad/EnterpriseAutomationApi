cb42482967ac7217eaa0074470de2aa992fb15ac
HosseinMolaei <hossein.molaei.backend@gmail.com>
2025-10-08 21:06:15 +0330

    Add Extention for login

---
+++ EnterpriseAutomation.Application/Extentions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+
+namespace EnterpriseAutomation.Application.Extentions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                ?? principal.FindFirst("sub")?.Value;
+        }
+
+        public static string GetEmail(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirst(ClaimTypes.Email)?.Value
+                ?? principal.FindFirst("email")?.Value;
+        }
+
+        public static List<string> GetRoles(this ClaimsPrincipal principal)
+        {
+            return principal.FindAll(ClaimTypes.Role)
+                .Select(c => c.Value)
+                .ToList();
+        }
+    }
+}
